71731e91a3f7ca75a8adfd9b25a39dc7479da871
Ihar-Batura <A17331582@gmail.com>
2025-03-03 19:11:43 +0300

    feat: add interfaces for IOption, IOptions and ICreateInput

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,28 @@ export interface ICreateElement {
   text?: string;
   parent?: HTMLElement;
 }
+
+interface IOption {
+  id: string;
+  title: string;
+  weight: string;
+}
+
+export interface IOptions {
+  list: IOption[] | [];
+  lastId: number;
+}
+
+export interface ICreateInput {
+  type: string;
+  classes?: string[];
+  id?: string;
+  title?: string;
+  required?: string;
+  placeholder?: string;
+  minLength?: string;
+  pattern?: string;
+  addRules?: string;
+  onInput?: () => void;
+  parent?: HTMLElement;
+}
