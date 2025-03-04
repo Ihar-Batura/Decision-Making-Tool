1dc024a19dc82a0ef6c3c635a6c1abd7221ace02
Ihar-Batura <A17331582@gmail.com>
2025-03-04 14:44:59 +0300

    feat: add check on empty array in function saveOptionsListToLS

--- src/functional/local_storage/save_options_list_to_LS.ts
+++ src/functional/local_storage/save_options_list_to_LS.ts
@@ -2,6 +2,10 @@ import { IOptions } from '../../types/types';
 
 function saveOptionsListToLS({ list, lastId }: IOptions): void {
   const data = { list: list, lastId: lastId };
+  if (list.length === 0) {
+    data.lastId = 0;
+  }
+
   localStorage.setItem('OptionsListDMT', JSON.stringify(data));
 }
 
