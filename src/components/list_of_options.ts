38d76b3d80ff10022f59dd3b185aa6e380812ec1
Ihar-Batura <A17331582@gmail.com>
2025-03-04 12:08:30 +0300

    feat: add function createListOfOptions

---
+++ src/components/list_of_options.ts
@@ -0,0 +1,29 @@
+import createElement from '../create/create_element';
+import checkOptionsInLS from '../functional/local_storage/check_options_in_LS';
+import getOptionsListFromLS from '../functional/local_storage/get_options_list_from_LS';
+import createOption from './option';
+
+function createListOfOptions(): HTMLElement {
+  const listOfOptions = createElement({ tag: 'div', classes: ['list-options'] });
+
+  checkOptionsInLS();
+
+  const options = getOptionsListFromLS();
+
+  if (options !== null && options.list.length > 0) {
+    console.log(options.list.length);
+    options.list.forEach((element) => {
+      const elementOption: HTMLElement = createOption({
+        id: element.id,
+        title: element.title,
+        weight: element.weight,
+      });
+
+      listOfOptions.append(elementOption);
+    });
+  }
+
+  return listOfOptions;
+}
+
+export default createListOfOptions;
