de4577b9ae65c07ad1b91ac089647cddd0a49ab0
Ihar-Batura <A17331582@gmail.com>
2025-03-04 17:16:07 +0300

    refactor: make better function clickBtnClearList

--- src/functional/click_button.ts/click_btn_clear_list.ts
+++ src/functional/click_button.ts/click_btn_clear_list.ts
@@ -1,13 +1,9 @@
 import saveOptionsListToLS from '../local_storage/save_options_list_to_LS';
-import cleanMain from '../clean_element_or_page/clean_main';
-import createPageListOfOptions from '../../pages/page_list_of_options';
-import { main } from '../../main';
+import reloadMainOnPageListOfOptions from '../reload_page/reload_main_on_page_list_of_options';
 
 function clickBtnClearList(): void {
   saveOptionsListToLS({ list: [], lastId: 0 });
-  cleanMain();
-  const pageListOfOptions: HTMLElement = createPageListOfOptions();
-  main.append(pageListOfOptions);
+  reloadMainOnPageListOfOptions();
 }
 
 export default clickBtnClearList;
