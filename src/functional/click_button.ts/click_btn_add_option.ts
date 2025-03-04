fc95cb3a0331dd824373be695deea983f0e8fa1f
Ihar-Batura <A17331582@gmail.com>
2025-03-04 17:16:30 +0300

    refactor: make better function clickBtnAddOption

--- src/functional/click_button.ts/click_btn_add_option.ts
+++ src/functional/click_button.ts/click_btn_add_option.ts
@@ -1,13 +1,9 @@
 import addNewOption from '../work_with_options/add_new_option';
-import cleanMain from '../clean_element_or_page/clean_main';
-import createPageListOfOptions from '../../pages/page_list_of_options';
-import { main } from '../../main';
+import reloadMainOnPageListOfOptions from '../reload_page/reload_main_on_page_list_of_options';
 
 function clickBtnAddOption(): void {
   addNewOption();
-  cleanMain();
-  const pageListOfOptions: HTMLElement = createPageListOfOptions();
-  main.append(pageListOfOptions);
+  reloadMainOnPageListOfOptions();
 }
 
 export default clickBtnAddOption;
