ecbd08924ed2ff9164d911e42594fc1ec9ed4098
Ihar-Batura <A17331582@gmail.com>
2025-03-05 18:06:45 +0300

    feat: add function clickBtnConfirm

---
+++ src/functional/click_button.ts/click_btn_confirm.ts
@@ -0,0 +1,28 @@
+import addNewOption from '../work_with_options/add_new_option';
+import reloadMainOnPageListOfOptions from '../reload_page/reload_main_on_page_list_of_options';
+
+function clickBtnConfirm(text: string): void {
+  const trimText: string = text.trim();
+
+  if (trimText.length < 3) return;
+
+  const arrNewOptions: string[] = trimText.split('\n');
+
+  arrNewOptions.forEach((el, ind) => {
+    const arrFromEl: string[] = el.split(',');
+
+    if (arrFromEl.length > 1) {
+      const elWeight = arrFromEl.at(-1)?.trim();
+
+      if (elWeight) {
+        const titleLength: number = arrNewOptions[ind].length - elWeight.length - 1;
+        const elTitle = arrNewOptions[ind].slice(0, titleLength);
+        addNewOption(elTitle, elWeight);
+      }
+    }
+  });
+
+  reloadMainOnPageListOfOptions();
+}
+
+export default clickBtnConfirm;
