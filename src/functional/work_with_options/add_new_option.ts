4ba65997be0ed06b8b61f84fe23bd51dc720ef71
Ihar-Batura <A17331582@gmail.com>
2025-03-05 18:06:05 +0300

    feat: add default value and check weight is number

--- src/functional/work_with_options/add_new_option.ts
+++ src/functional/work_with_options/add_new_option.ts
@@ -2,13 +2,17 @@ import getOptionsListFromLS from '../local_storage/get_options_list_from_LS';
 import saveOptionsListToLS from '../local_storage/save_options_list_to_LS';
 import { IOptions, IOption } from '../../types/types';
 
-function addNewOption(): void {
+function addNewOption(title: string = '', weight: string = ''): void {
   const options: IOptions | null = getOptionsListFromLS();
   if (options !== null) {
     const optionsList: IOption[] = options.list;
     const optionsLastId: number = options.lastId;
     const newLastId: number = optionsLastId + 1;
-    const newOption = { id: `#${newLastId}`, title: '', weight: '' };
+    const newOption = {
+      id: `#${newLastId}`,
+      title: title,
+      weight: `${typeof +weight === 'number' ? weight : ''}`,
+    };
 
     optionsList.push(newOption);
 
