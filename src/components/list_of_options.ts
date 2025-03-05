441550b094248c98daba74e7e120d7df919ca427
Ihar-Batura <A17331582@gmail.com>
2025-03-05 18:07:37 +0300

    refactor: add some types for function createListOfOptions

--- src/components/list_of_options.ts
+++ src/components/list_of_options.ts
@@ -1,6 +1,7 @@
 import createElement from '../create/create_element';
 import checkOptionsInLS from '../functional/local_storage/check_options_in_LS';
 import getOptionsListFromLS from '../functional/local_storage/get_options_list_from_LS';
+import { IOptions, IOption } from '../types/types';
 import createOption from './option';
 
 function createListOfOptions(): HTMLElement {
@@ -8,10 +9,10 @@ function createListOfOptions(): HTMLElement {
 
   checkOptionsInLS();
 
-  const options = getOptionsListFromLS();
+  const options: IOptions | null = getOptionsListFromLS();
 
   if (options !== null && options.list.length > 0) {
-    options.list.forEach((element) => {
+    options.list.forEach((element: IOption) => {
       const elementOption: HTMLElement = createOption({
         id: element.id,
         title: element.title,
