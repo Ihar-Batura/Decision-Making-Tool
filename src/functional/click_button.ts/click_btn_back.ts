d91d12024a1f9838cb3dd802c3360a76339554c0
Ihar-Batura <A17331582@gmail.com>
2025-03-06 14:00:31 +0300

    feat: add function clickBtnBack

---
+++ src/functional/click_button.ts/click_btn_back.ts
@@ -0,0 +1,7 @@
+import reloadMainOnPageListOfOptions from '../reload_page/reload_main_on_page_list_of_options';
+
+function clickBtnBack(): void {
+  reloadMainOnPageListOfOptions();
+}
+
+export default clickBtnBack;
