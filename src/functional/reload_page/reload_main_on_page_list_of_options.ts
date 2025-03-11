f4801190598e71270458bbbd33050e19b6a37b53
Ihar-Batura <A17331582@gmail.com>
2025-03-11 19:21:30 +0300

    feat: add call function routerPath in function reloadMainOnPageListOfOptions

--- src/functional/reload_page/reload_main_on_page_list_of_options.ts
+++ src/functional/reload_page/reload_main_on_page_list_of_options.ts
@@ -1,9 +1,11 @@
 import { main } from '../../main';
 import cleanMain from '../clean_element_or_page/clean_main';
 import createPageListOfOptions from '../../pages/page_list_of_options';
+import routerPath from '../../router/router_path';
 
 function reloadMainOnPageListOfOptions(): void {
   cleanMain();
+  routerPath('#/');
   const pageListOfOptions: HTMLElement = createPageListOfOptions();
   main.append(pageListOfOptions);
 }
