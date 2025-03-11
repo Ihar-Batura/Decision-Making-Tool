8ae1916c96361b9762667e9786bb17036148364c
Ihar-Batura <A17331582@gmail.com>
2025-03-11 19:20:03 +0300

    feat: add input for timer

--- src/components/buttons_for_decision_picker.ts
+++ src/components/buttons_for_decision_picker.ts
@@ -1,5 +1,6 @@
 import createElement from '../create/create_element';
 import createButton from '../create/create_button';
+import createInput from '../create/create_input';
 import clickBtnBack from '../functional/click_button.ts/click_btn_back';
 import clickBtnSound from '../functional/click_button.ts/click_btn_sound';
 import checkSoundValueInLS from '../functional/local_storage/check_sound_value_in_LS';
@@ -16,12 +17,27 @@ function createButtonsForDecisionPicker(): HTMLElement {
     parent: btnsContainer,
   });
 
-  createButton({
-    classes: ['btn', 'btn-time'],
-    text: 'Time',
-    onClick: () => console.log('click btn time'),
+  const timerContainer = createElement({
+    tag: 'div',
+    classes: ['timer-container'],
     parent: btnsContainer,
   });
+
+  createElement({
+    tag: 'div',
+    classes: ['timer-img'],
+    parent: timerContainer,
+  });
+
+  const timerInput = createInput({
+    type: 'number',
+    classes: ['timer-input'],
+    value: '5',
+    placeholder: 'Seconds',
+    parent: timerContainer,
+  });
+
+  timerInput.setAttribute('min', '5');
 
   checkSoundValueInLS();
 
