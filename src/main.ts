2e2e83e94206b1ad35f8e8fcdd41b08901d2fd17
Ihar-Batura <A17331582@gmail.com>
2025-03-11 19:19:34 +0300

    feat: add call function routerPage in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './sass/style.scss';
 import createHeader from './components/header';
 import createMain from './components/main';
 import createFooter from './components/footer';
-import createPageListOfOptions from './pages/page_list_of_options';
+import routerPage from './router/router_page';
 
 export const body: HTMLElement = document.body;
 const header: HTMLElement = createHeader();
 export const main: HTMLElement = createMain();
-const pageListOfOptions: HTMLElement = createPageListOfOptions();
-main.append(pageListOfOptions);
 const footer: HTMLElement = createFooter();
 
 body.append(header, main, footer);
+
+routerPage();
