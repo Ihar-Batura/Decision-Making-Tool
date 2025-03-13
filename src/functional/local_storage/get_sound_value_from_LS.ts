466d9e6713a8aa214df3ef309918255a00fb3ab8
Ihar-Batura <A17331582@gmail.com>
2025-03-13 12:31:40 +0300

    refactor: improve get_sound_value_from__LS.ts

--- src/functional/local_storage/get_sound_value_from_LS.ts
+++ src/functional/local_storage/get_sound_value_from_LS.ts
@@ -2,8 +2,8 @@ import { ISound } from '../../types/types';
 
 function getSoundValueFromLS(): ISound | null {
   const soundValue: string | null = localStorage.getItem('SoundValueDMT');
-  if (soundValue) {
-    const value = JSON.parse(soundValue);
+  if (soundValue !== null) {
+    const value: ISound = JSON.parse(soundValue);
     return value;
   }
   return null;
