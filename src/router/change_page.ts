65eb84cf57a24fb8640f73c3d3b036af6431070d
Ihar-Batura <A17331582@gmail.com>
2025-03-13 12:35:32 +0300

    feat: add new functional to function changePage

--- src/router/change_page.ts
+++ src/router/change_page.ts
@@ -3,21 +3,34 @@ import createPageListOfOptions from '../pages/page_list_of_options';
 import createPageDecisionPicker from '../pages/page_decision_picker';
 import createPageError from '../pages/page_error';
 import { main } from '../main';
+import findValidOptions from '../functional/work_with_options/find_valid_options';
+import { IOption } from '../types/types';
+import routerPath from './router_path';
 
 function changePage(): void {
   const hash: string = window.location.hash;
 
   cleanMain();
 
-  if (hash === '#/') {
+  if (hash === '#/' || hash === '') {
     const pageListOfOptions: HTMLElement = createPageListOfOptions();
     main.append(pageListOfOptions);
   }
+
   if (hash === '#/decision-picker') {
-    const pageDecisionPicker: HTMLElement = createPageDecisionPicker();
-    main.append(pageDecisionPicker);
+    const validOptions: IOption[] = findValidOptions();
+
+    if (validOptions.length >= 2) {
+      const pageDecisionPicker: HTMLElement = createPageDecisionPicker();
+      main.append(pageDecisionPicker);
+    } else {
+      const pageListOfOptions: HTMLElement = createPageListOfOptions();
+      main.append(pageListOfOptions);
+      routerPath('#/');
+    }
   }
-  if (hash !== '#/' && hash !== '#/decision-picker') {
+
+  if (hash !== '#/' && hash !== '#/decision-picker' && hash !== '') {
     const pageError: HTMLElement = createPageError();
     main.append(pageError);
   }
