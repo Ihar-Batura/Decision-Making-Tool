c547dae3254ac8be55f83b3eb82de006c1e885ac
Ihar-Batura <A17331582@gmail.com>
2025-03-13 12:24:16 +0300

    refactor: refactor wheel.ts

--- src/components/wheel.ts
+++ src/components/wheel.ts
@@ -1,16 +1,21 @@
-import createElement from '../create/create_element';
+import createCanvasElement from '../create/create_canvas';
 import drawBigCircle from '../wheel/draw_big_circle';
 import drawCursor from '../wheel/draw_cursor';
 import drawSmallCircle from '../wheel/draw_small_circle';
+import drawOptions from '../wheel/draw_options';
 
 function createWheel(): HTMLCanvasElement {
-  const canvas = createElement({ tag: 'canvas', classes: ['wheel-canvas'] }) as HTMLCanvasElement;
-  canvas.width = 512;
-  canvas.height = 512;
+  const canvas: HTMLCanvasElement = createCanvasElement({
+    classes: ['wheel-canvas'],
+    width: 512,
+    height: 512,
+  });
+
   const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
 
   if (ctx !== null) {
     drawBigCircle(ctx);
+    drawOptions(ctx);
     drawSmallCircle(ctx);
     drawCursor(ctx);
   }
