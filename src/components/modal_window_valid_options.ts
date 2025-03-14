048dbbd5c0871d2764bab37c4aab6429d0f57a15
Ihar-Batura <A17331582@gmail.com>
2025-03-14 17:45:55 +0300

    fix: fix function createModalWindowValidOptions delete 'as'

--- src/components/modal_window_valid_options.ts
+++ src/components/modal_window_valid_options.ts
@@ -39,7 +39,12 @@ function createModalWindowValidOptions(): HTMLDialogElement {
   });
 
   document.addEventListener('click', (event: MouseEvent) => {
-    const element: HTMLElement = event.target as HTMLElement;
+    const target: EventTarget | null = event.target;
+    if (!(target instanceof HTMLElement)) {
+      return;
+    }
+
+    const element: HTMLElement = target;
     if (element.classList.contains('modal-window')) {
       clickBtnCancel(dialogWindow);
     }
