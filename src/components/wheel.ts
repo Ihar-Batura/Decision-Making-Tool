0e79ccff12f037fd8ab2e555f62570a9efb1fb15
Ihar-Batura <A17331582@gmail.com>
2025-03-14 17:24:16 +0300

    feat: add animate when when click button start

--- src/components/wheel.ts
+++ src/components/wheel.ts
@@ -3,8 +3,16 @@ import drawBigCircle from '../wheel/draw_big_circle';
 import drawCursor from '../wheel/draw_cursor';
 import drawSmallCircle from '../wheel/draw_small_circle';
 import drawOptions from '../wheel/draw_options';
+import { IOption } from '../types/types';
+import animateWheel from '../wheel/animate_wheel';
 
-function createWheel(): HTMLCanvasElement {
+function createWheel(
+  mixArrOptions: IOption[],
+  arrOptionColors: string[],
+  btnStart: HTMLButtonElement,
+  input: HTMLInputElement,
+  pickedOption: HTMLElement
+): HTMLCanvasElement {
   const canvas: HTMLCanvasElement = createCanvasElement({
     classes: ['wheel-canvas'],
     width: 512,
@@ -12,12 +20,20 @@ function createWheel(): HTMLCanvasElement {
   });
 
   const ctx: CanvasRenderingContext2D | null = canvas.getContext('2d');
+  const offsetAngle: number = 0;
 
   if (ctx !== null) {
     drawBigCircle(ctx);
-    drawOptions(ctx);
+    drawOptions(ctx, mixArrOptions, arrOptionColors, offsetAngle);
     drawSmallCircle(ctx);
     drawCursor(ctx);
+
+    if (btnStart !== null && input !== null) {
+      btnStart.addEventListener('click', (event) => {
+        event.preventDefault();
+        animateWheel(ctx, mixArrOptions, arrOptionColors, input, pickedOption);
+      });
+    }
   }
   return canvas;
 }
